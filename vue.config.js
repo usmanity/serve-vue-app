df2b197a8f5aaf4c38a0c34f1d07d402c7e6640d
Muhammad Usman <muhammad@fastmail.com>
2019-04-13 09:40:39 -0700

    add vue.config.js file

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // this will be the URL at which it will be served from
+  // if you're using something like example.com, set it to `/`
+  // for my example, I'm doing this b/c I already have something at `/`
+  publicPath: "/sample-vue-app",
+  // by default this is `dist` which is not what we want here
+  // github lets you serve the `docs` folder as a github page
+  outputDir: "docs"
+};
