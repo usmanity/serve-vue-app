3c81e4bdb32e6dd89d343fee5a0c1dde1d7b407c
Muhammad Usman <muhammad@fastmail.com>
2019-04-13 09:52:45 -0700

    oops used sample instead of 'serve' for the app name

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // this will be the URL at which it will be served from
   // if you're using something like example.com, set it to `/`
   // for my example, I'm doing this b/c I already have something at `/`
-  publicPath: "/sample-vue-app",
+  publicPath: "/serve-vue-app",
   // by default this is `dist` which is not what we want here
   // github lets you serve the `docs` folder as a github page
   outputDir: "docs"
